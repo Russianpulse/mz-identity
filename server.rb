e978e4f7be0ffb34d77f9543747414f99215a052
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-09-26 13:03:19 +0300

    tabs go away

--- server.rb
+++ server.rb
@@ -4,25 +4,25 @@ require 'omniauth-vkontakte'
 
 
 class Server < Sinatra::Base
-	use Rack::Session::Cookie
-	use OmniAuth::Builder do
-		provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'],
-			{
-				scope:      'friends,audio',
-				display:    'popup',
-				lang:       'en',
-				image_size: 'original'
-			}
-	end
-
-  get '/' do
-		<<-HTML
-      <a href='/auth/vkontakte'>Sign in with VK</a>
-		HTML
+  use Rack::Session::Cookie
+  use OmniAuth::Builder do
+    provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'],
+      {
+        scope:      'friends,audio',
+        display:    'popup',
+        lang:       'en',
+        image_size: 'original'
+      }
   end
 
-	get '/auth/:provider/callback' do
-		content_type 'text/plain'
-		request.env['omniauth.auth'].info.to_hash.inspect
-	end
+  get '/' do
+    <<-HTML
+      <a href='/auth/vkontakte'>Sign in with VK</a>
+    HTML
+  end
+
+  get '/auth/:provider/callback' do
+    content_type 'text/plain'
+    request.env['omniauth.auth'].info.to_hash.inspect
+  end
 end
