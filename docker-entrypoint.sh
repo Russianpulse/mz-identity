20de0aff06489146eba1d4c43ac32b91da99b173
Sergei O. Udalov <sergei.udalov@gmail.com>
2017-09-26 11:09:02 +0300

    sinatra test

---
+++ docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+if [ "$1" = 'web' ]; then
+  rm -rf tmp/pids/*
+  bundle exec rackup -p 80 -host 0.0.0.0
+fi
+
+exec "$@"
---
+++ server.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'omniauth'
+require 'omniauth-vkontakte'
+
+
+class Server < Sinatra::Base
+	use Rack::Session::Cookie
+	use OmniAuth::Builder do
+		provider :vkontakte, ENV['VK_API_KEY'], ENV['VK_API_SECRET'],
+			{
+				scope:      'friends,audio',
+				display:    'popup',
+				lang:       'en',
+				image_size: 'original'
+			}
+	end
+
+  get '/' do
+		<<-HTML
+      <a href='/auth/vkontakte'>Sign in with VK</a>
+		HTML
+  end
+
+	get '/auth/:provider/callback' do
+		content_type 'text/plain'
+		request.env['omniauth.auth'].info.to_hash.inspect
+	end
+end
